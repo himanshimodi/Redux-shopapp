122441bf091bde3823e9b08f78039254d2efb1b7
himanshimodi <138893449+himanshimodi@users.noreply.github.com>
2024-04-04 18:26:45 +0530

    Update CartSlice.jsx

--- src/redux/Slices/CartSlice.jsx
+++ src/redux/Slices/CartSlice.jsx
@@ -4,6 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const CartSlice = createSlice({
     name:"cart",
     initialState:[],
+    //REDUCER IS AN OBJECT THAT CONTAINS FUNCTIONS WHICH ARE REQUIRED TO HAVE A STATE & ACTION TO WORK
     reducers:{
         add:(state,action) => {
             state.push(action.payload);
